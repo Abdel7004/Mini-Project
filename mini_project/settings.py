bc550338df5abc0bf898e65547e2035494492152
Abdel7004 <abdelrahmanh15@gmail.com>
2023-07-06 21:39:19 -0400

    final

--- mini_project/settings.py
+++ mini_project/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app', # our application here.
-    'first.apps.FirstConfig',
+    # 'first.apps.FirstConfig',
     'corsheaders',
 ]
 
